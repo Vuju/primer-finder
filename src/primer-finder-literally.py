b1960c2fcd8e18c53a72d0268d65c612303d2538
julian@vu-online.de <julian@vu-online.de>
2025-01-11 21:15:24 +0100

    Finished working on a naive Regex matching algorithm. It can find all reads with both primers in the reference file in 13-14 seconds.

--- src/primer-finder-literally.py
+++ src/primer-finder-literally.py
@@ -1,30 +1,62 @@
+import string
 import sys
+import re
+
+def regexBuilder(initialString):
+    regexString = ""
+    for char in initialString:
+        match char:
+            # skipping USMKRBVN since they dont appear in my primers (yet) (https://en.wikipedia.org/wiki/Nucleic_acid_notation)
+            case 'A':
+                regexString += "A"
+            case 'T':
+                regexString += "T"
+            case 'C':
+                regexString += "C"
+            case 'G':
+                regexString += "G"
+            case 'W':
+                regexString += "[AT]"
+            case 'Y':
+                regexString += "[CT]"
+            case 'D':
+                regexString += "[AGT]"
+            case 'H':
+                regexString += "[ACT]"
+            case _:
+                print(f"Unable to process primer. Didn't find {char}.")
+    return regexString
+
 
 if len(sys.argv) != 5:
     forward_primer = "GGDACWGGWTGAACWGTWTAYCCHCC"
     backward_primer = "CCWGTWYTAGCHGGDGCWATYAC"
-    primers = [forward_primer, backward_primer]
     input_file_path = './data/DB.COX1.fna'
     output_file_path = './data/output.txt'
-
 else:
     forward_primer = sys.argv[1]
     backward_primer = sys.argv[2]
     input_file_path = sys.argv[3]
     output_file_path = sys.argv[4]
 
-# Todo: convert primers to Regex or smth. Currently there are no matches as WHYD[...] will not appear in reads, obviously.
+forward_primer_regex = regexBuilder(forward_primer)
+print(forward_primer_regex)
+backward_primer_regex = regexBuilder(backward_primer)
+print(backward_primer_regex)
+primers = forward_primer_regex + ".*" + backward_primer_regex
+
+currentRead = ""
 
 with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
-
     for line_number, line in enumerate(input_file, start=1):
-        if line_number % 2 == 0:  # Check only Reads, not infos
-            if any(substring in line for substring in primers):
-                output_file.write(f"Line {line_number}: {line.strip()}\n")
-            else:
-                output_file.write(f"Line {line_number}: No Primer.\n")
+        if line_number % 2 == 1:
+            currentRead = line
         else:
-            output_file.write(line.strip())
+            matchAttempt = re.search(primers, line)
+            if matchAttempt is not None:
+                output_file.write(currentRead.replace('|', ';').replace(',', ';').strip() +
+                                  "".join([str(index) + ';' for index in matchAttempt.span()]) +
+                                  "\n")
 
-print(f"Counts of 'A' have been written to {output_file_path}")
 
+print(f"Output has been written to {output_file_path}")
