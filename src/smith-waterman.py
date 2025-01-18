ea6e6599e91192ae399807de7b9035defe801a91
julian@vu-online.de <julian@vu-online.de>
2025-01-18 13:36:07 +0100

    Extended smith-waterman.py with triplet jump functionality

--- src/smith-waterman.py
+++ src/smith-waterman.py
@@ -1,5 +1,4 @@
 
-# Plan 1: implement triplet insert/delete
 # Plan 2: reformat input parameters
 # Plan 3: rework the traceback, especially the output
 
@@ -17,9 +16,14 @@ def smith_waterman(primer, read, gap=-2, substitution_matrix=(lambda x, y: 2 if 
     for i in range(1, rows):
         for j in range(1, cols):
             match_score = score_matrix[i - 1][j - 1] + (substitution_matrix(primer[i - 1], read[j - 1]))
-            delete = score_matrix[i - 1][j] + gap
-            insert = score_matrix[i][j - 1] + gap
-            score_matrix[i][j] = max(0, match_score, delete, insert)
+            delete = score_matrix[i - 1][j] + (2 * gap)
+            insert = score_matrix[i][j - 1] + (2 * gap)
+
+            # calculate triplet jump value
+            del3 = (score_matrix[i - 3][j] + gap) if (i - 3 >= 0) else 0
+            ins3 = (score_matrix[i][j - 3] + gap) if (j - 3 >= 0) else 0
+
+            score_matrix[i][j] = max(0, match_score, delete, insert, del3, ins3)
 
             if score_matrix[i][j] > max_score:
                 max_score = score_matrix[i][j]
@@ -36,16 +40,27 @@ def smith_waterman(primer, read, gap=-2, substitution_matrix=(lambda x, y: 2 if 
             aligned_read.append(read[j - 1])
             i -= 1
             j -= 1
-        elif score_matrix[i][j] == score_matrix[i - 1][j] + gap:
+        elif score_matrix[i][j] == score_matrix[i - 1][j] + (2 * gap):
             aligned_primer.append(primer[i - 1])
             aligned_read.append('-')
             i -= 1
-        elif score_matrix[i][j] == score_matrix[i][j - 1] + gap:
+        elif score_matrix[i][j] == score_matrix[i][j - 1] + (2 * gap):
             aligned_primer.append('-')
             aligned_read.append(read[j - 1])
             j -= 1
+        elif (i - 3 >= 0) & score_matrix[i][j] == score_matrix[i - 3][j] + gap:
+            aligned_primer.append(primer[i - 3])
+            aligned_read.append('---')
+            i -= 3
+        elif (j - 3 >= 0) & score_matrix[i][j] == score_matrix[i][j - 3] + gap:
+            aligned_primer.append('---')
+            aligned_read.append(read[j - 3])
+            j -= 3
+        else:
+            raise Exception("Smith-Waterman traceback failed!")
 
     aligned_primer = ''.join(reversed(aligned_primer))
     aligned_read = ''.join(reversed(aligned_read))
+    alignment_start_index_in_read = j
 
-    return max_score, aligned_primer, aligned_read
+    return max_score, aligned_primer, aligned_read, alignment_start_index_in_read
