6f1479e83e962169074bbfcecd294f2e80128fd9
julian@vu-online.de <julian@vu-online.de>
2025-01-19 19:17:44 +0100

    finished implementing a new primer finder. It takes about 16 hours to run. Too long.

--- src/primer_finder.py
+++ src/primer_finder.py
@@ -1,24 +1,33 @@
 import sys
 from smith_waterman import smith_waterman
 
-# Task 1: Implement write-back
-# Task 2 : Implement proper substitution function
 
-def substitution_function():
-    pass
+# Task 3: Implement additional gzip support
+
+def substitution_function(p, r):
+    match r:
+        case 'A':
+            return 2 if p in "AWMRDHVN" else -1
+        case 'C':
+            return 2 if p in "CSMYBHVN" else -1
+        case 'G':
+            return 2 if p in "GSKRBDVN" else -1
+        case 'T':
+            return 2 if p in "TWKYBDHN" else -1
+        case default:
+            raise Exception(f"unknown literal in read sequence: '{r}'")
 
 
 if len(sys.argv) != 5:
     forward_primer = "GGDACWGGWTGAACWGTWTAYCCHCC"
     backward_primer = "CCWGTWYTAGCHGGDGCWATYAC"
     input_file_path = './data/DB.COX1.fna'
-    output_file_path = './data/double-primer-matching.txt'
+    output_file_path = './data/primer-finder.csv'
 else:
     forward_primer = sys.argv[1]
     backward_primer = sys.argv[2]
     input_file_path = sys.argv[3]
     output_file_path = sys.argv[4]
-
 
 currentRead = ""
 
@@ -27,7 +36,10 @@ with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as ou
         if line_number % 2 == 1:
             currentRead = line
         else:
-            f_score, f_primer, f_read, f_index = smith_waterman(forward_primer, line, -2, substitution_function)
-            b_score, b_primer, b_read, b_index = smith_waterman(backward_primer, line, -2, substitution_function)
+            read = line.strip()
+            f_score, f_primer, f_read, f_index = smith_waterman(forward_primer, read, -2, substitution_function)
+            b_score, b_primer, b_read, b_index = smith_waterman(backward_primer, read, -2, substitution_function)
+            output_file.write(currentRead.replace('|', ';').replace(',', ';').strip() +
+                              f"{f_score};{f_primer};{f_index};{b_score};{b_primer};{b_index}\n")
 
 print(f"Output has been written to {output_file_path}")
--- src/smith_waterman.py
+++ src/smith_waterman.py
@@ -1,14 +1,15 @@
 
 
-def smith_waterman(primer, read, gap=-2, substitution_matrix=(lambda x, y: 2 if (x == y) else -1)):
+def smith_waterman(primer, read, gap=-2, substitution_function=(lambda p, r: 2 if (p == r) else -1)):
     """
     An implementation of the Smith-Waterman algorithm for local sequence alignment.
     Extended with the functionality to jump triplets.
     :param primer: The first sequence. In my case a primer, hence the name.
     :param read: The second sequence. In this case the read in which I want to find my primer sequence.
     :param gap: The penalty value for skipping a triplet. Default -2.
-    :param substitution_matrix: The reward and penalty values from comparing two characters.
-    Has to be a function at the moment. Default +2 for match, -1 for mismatch.
+    :param substitution_function: The reward and penalty values from comparing two characters.
+    Has to be a function that accepts two characters to compare and returns a numerical value.
+    By default it returns +2 for a match, -1 for a mismatch.
     :return max_score: the highest score in the final score-matrix
     :return aligned_primer:the primer sequence with potential skips
     :return aligned_read: the read sequence with potential skips
@@ -25,7 +26,7 @@ def smith_waterman(primer, read, gap=-2, substitution_matrix=(lambda x, y: 2 if 
 
     for i in range(1, rows):
         for j in range(1, cols):
-            match_score = score_matrix[i - 1][j - 1] + (substitution_matrix(primer[i - 1], read[j - 1]))
+            match_score = score_matrix[i - 1][j - 1] + (substitution_function(primer[i - 1], read[j - 1]))
             delete = score_matrix[i - 1][j] + (2 * gap)
             insert = score_matrix[i][j - 1] + (2 * gap)
 
@@ -45,7 +46,7 @@ def smith_waterman(primer, read, gap=-2, substitution_matrix=(lambda x, y: 2 if 
     i, j = max_pos
 
     while i > 0 and j > 0 and score_matrix[i][j] > 0:
-        if score_matrix[i][j] == score_matrix[i - 1][j - 1] + (substitution_matrix(primer[i - 1], read[j - 1])):
+        if score_matrix[i][j] == score_matrix[i - 1][j - 1] + (substitution_function(primer[i - 1], read[j - 1])):
             aligned_primer.append(primer[i - 1])
             aligned_read.append(read[j - 1])
             i -= 1
@@ -58,11 +59,11 @@ def smith_waterman(primer, read, gap=-2, substitution_matrix=(lambda x, y: 2 if 
             aligned_primer.append('-')
             aligned_read.append(read[j - 1])
             j -= 1
-        elif (i - 3 >= 0) & score_matrix[i][j] == score_matrix[i - 3][j] + gap:
+        elif (i - 3 >= 0) and (score_matrix[i][j] == score_matrix[i - 3][j] + gap):
             aligned_primer.append(primer[i - 3])
             aligned_read.append('---')
             i -= 3
-        elif (j - 3 >= 0) & score_matrix[i][j] == score_matrix[i][j - 3] + gap:
+        elif (j - 3 >= 0) and (score_matrix[i][j] == score_matrix[i][j - 3] + gap):
             aligned_primer.append('---')
             aligned_read.append(read[j - 3])
             j -= 3
