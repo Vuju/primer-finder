0b7f1b86aae46f130cf4732effed87ada311ccbf
julian@vu-online.de <julian@vu-online.de>
2025-02-13 16:01:04 +0100

    updated orf finder to be extended soon

--- src/orf_finder.py
+++ src/orf_finder.py
@@ -1,12 +1,19 @@
 from Bio.Seq import Seq
 
+
 def find_orf(sequence):
+    possibilities = list_possible_orf(sequence)
+    if possibilities:
+        return possibilities[0]
+    return -1
+
+def list_possible_orf(sequence):
     dna = Seq(sequence)
-    # Translate using mitochondrial invertebrate code (table 5)
+    orf_list = []
     for frame in range(3):
         framed_seq = dna[frame:]
         # Translate using invertebrate mitochondrial code
         protein = framed_seq.translate(table=5)
         if '*' in protein:
-            return frame
-    return -1
+            orf_list.append(frame)
+    return orf_list
