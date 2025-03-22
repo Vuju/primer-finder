7294f30e6c11938429c0db45d8e6c8333bcfed3c
Julian Siemer <julian@vu-online.de>
2025-03-22 14:01:45 +0100

    added trailing "N" in orf_finder, so Bioconda doesn't throw warnings all the time.

--- src/orf_finder.py
+++ src/orf_finder.py
@@ -1,7 +1,5 @@
 from Bio.Seq import Seq
 
-# todo: extend codons to multiples of 3, because Biopython wants me to.
-
 def find_orf(sequence):
     possibilities = list_possible_orf(sequence)
     if possibilities:
@@ -13,6 +11,13 @@ def list_possible_orf(sequence):
     orf_list = []
     for frame in range(3):
         framed_seq = dna[frame:]
+
+        # adding trailing N, because Bio is pissed if I don't
+        remainder = len(framed_seq) % 3
+        if remainder > 0:
+            for i in range(3 - remainder):
+                framed_seq += "N"
+
         # Translate using invertebrate mitochondrial code
         protein = framed_seq.translate(table=5)
         if '*' in protein:
